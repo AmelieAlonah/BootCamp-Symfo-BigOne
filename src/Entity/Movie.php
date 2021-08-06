1531c6e41ef3365c8a8f4c92c72aa3d56b81cacf
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-08-06 20:55:07 +0200

    Controller and Entity Movie OK

---
+++ src/Entity/Movie.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
+ */
+class Movie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $genres;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $casting;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $reviews;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $releaseDate;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $poster;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $rating;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getGenres(): ?string
+    {
+        return $this->genres;
+    }
+
+    public function setGenres(string $genres): self
+    {
+        $this->genres = $genres;
+
+        return $this;
+    }
+
+    public function getCasting(): ?string
+    {
+        return $this->casting;
+    }
+
+    public function setCasting(string $casting): self
+    {
+        $this->casting = $casting;
+
+        return $this;
+    }
+
+    public function getReviews(): ?string
+    {
+        return $this->reviews;
+    }
+
+    public function setReviews(string $reviews): self
+    {
+        $this->reviews = $reviews;
+
+        return $this;
+    }
+
+    public function getReleaseDate(): ?\DateTimeInterface
+    {
+        return $this->releaseDate;
+    }
+
+    public function setReleaseDate(\DateTimeInterface $releaseDate): self
+    {
+        $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getPoster(): ?string
+    {
+        return $this->poster;
+    }
+
+    public function setPoster(string $poster): self
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
