5c237cdfa4f33810f0d65c3657c18ce0fbff73ca
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-08-07 13:23:36 +0200

    Create entity genre

---
+++ src/Entity/Genre.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GenreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GenreRepository::class)
+ */
+class Genre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $movies;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMovies(): ?string
+    {
+        return $this->movies;
+    }
+
+    public function setMovies(string $movies): self
+    {
+        $this->movies = $movies;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
